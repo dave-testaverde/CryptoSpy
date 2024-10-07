543aa75393759ff295fb0d72517db077df0e4261
dave-testaverde <davidetestaverde@gmail.com>
2024-10-07 16:00:46 +0200

    Optimized currency list tests from ModelContainer

--- CryptoSpyTests/ListSection/Persistance/CurrenciesDataSourceTests.swift
+++ CryptoSpyTests/ListSection/Persistance/CurrenciesDataSourceTests.swift
@@ -14,14 +14,20 @@ final class CurrenciesDataSourceTests: XCTestCase {
     static let currencies = Currencies(listSupported: ["usd", "eur", "gbp"])
     
     func testGetCurrencies_whenLoadingCurrenciesIsSuccessful_getSuccessfulResponse() {
-        let sut = makeSUT()
+        let sut = makeSUT(populate: true)
         let getCryptoResult = sut.loadItems()
         XCTAssertEqual(Result.success([Self.currencies]), getCryptoResult)
     }
     
     func testGetEmptyResponse_whenDeletingAllCurrencies() {
-        let sut = makeSUT()
+        let sut = makeSUT(populate: true)
         sut.removeAll()
+        let getCryptoResult = sut.loadItems()
+        XCTAssertEqual(Result.success([]), getCryptoResult)
+    }
+    
+    func testGetEmptyResponse_whenNotPopulated() {
+        let sut = makeSUT(populate: false)
         let getCryptoResult = sut.loadItems()
         XCTAssertEqual(Result.success([]), getCryptoResult)
     }
@@ -29,11 +35,13 @@ final class CurrenciesDataSourceTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(
+        populate: Bool = false,
         file: StaticString = #file,
         line: UInt = #line
     ) -> CurrenciesDataSource {
-        let sut = CurrenciesDataSource.shared
-        sut.appendItem(item: Self.currencies)
-        return sut
+        if(populate){
+            CurrenciesDataSource.shared.appendItem(item: Self.currencies)
+        }
+        return CurrenciesDataSource.shared
     }
 }
